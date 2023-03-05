91326159f7431fb56e5bcff36c33777587400a69
wendy-fgt <chinwendu.onuoha@flutterwavego.com>
2023-03-05 11:40:54 +0100

    answer model creation

---
+++ src/answer/answer.model.ts
@@ -0,0 +1,52 @@
+import { Optional } from 'sequelize';
+import {
+  AutoIncrement,
+  BelongsTo,
+  Column,
+  DataType,
+  ForeignKey,
+  Model,
+  PrimaryKey,
+  Table,
+} from 'sequelize-typescript';
+import { Person } from 'src/auth/person.model';
+import { Question } from 'src/question/question.model';
+
+interface AnswerAttributes {
+  id: number;
+  body: string;
+  person_id: number;
+  question_id: number;
+}
+
+@Table({
+  tableName: 'answer',
+  createdAt: 'created_at',
+  updatedAt: 'updated_at',
+})
+export class Answer extends Model<
+  AnswerAttributes,
+  Optional<AnswerAttributes, 'id'>
+> {
+  @AutoIncrement
+  @PrimaryKey
+  @Column
+  id?: number;
+
+  @Column({
+    type: DataType.TEXT,
+    allowNull: false,
+  })
+  body: string;
+
+  @ForeignKey(() => Person)
+  @Column
+  person_id: number;
+
+  @ForeignKey(() => Question)
+  @Column
+  question_id: number;
+
+  @BelongsTo(() => Person)
+  author: Person;
+}
