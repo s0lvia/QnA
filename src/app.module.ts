e2210b24331d0b2cf9af99ffd04e75b819c12dc4
wendy-fgt <chinwendu.onuoha@flutterwavego.com>
2023-03-07 07:43:49 +0100

    comment module setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { sequelizeConfigOpts } from './config/sequelize.config';
 import { AuthModule } from './auth/auth.module';
 import { QuestionModule } from './question/question.module';
 import { AnswerModule } from './answer/answer.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { AnswerModule } from './answer/answer.module';
     AuthModule,
     QuestionModule,
     AnswerModule,
+    CommentModule,
   ],
   controllers: [AppController],
   providers: [AppService],
